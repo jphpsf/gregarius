86c09b6b34f25fbf92ec080b550a94f5267b7273
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2004-06-01 08:06:28 +0000

    turned verbose error reporting off again
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@36 d826556c-87f9-0310-90b4-9554b5b11594

--- init.php
+++ init.php
@@ -25,7 +25,7 @@
 #
 ###############################################################################
 
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 
 require_once('config.php');
 require_once('db.php');
