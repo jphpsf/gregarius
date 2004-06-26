4f34c0cd4a697031318064b01fffbb03285c0a16
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2004-06-26 11:36:53 +0000

    french labels
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@84 d826556c-87f9-0310-90b4-9554b5b11594

---
+++ intl/fr.php
@@ -0,0 +1,93 @@
+<?php
+###############################################################################
+# Gregarius - A PHP based RSS aggregator.
+# Copyright (C) 2003, 2004 Marco Bonetti
+#
+###############################################################################
+# This program is free software and open source software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
+# http://www.gnu.org/licenses/gpl.html
+#
+###############################################################################
+# E-mail:      mbonetti at users dot sourceforge dot net
+# Web page:    http://sourceforge.net/projects/gregarius
+#
+###############################################################################
+
+
+
+
+
+define ('MARK_READ', "Tous marquer comme lus");
+define ('MARK_CHANNEL_READ', "Marquer ce canal comme lu");
+define ('SEE_ALL_UNREAD', "Voir tous les %d éléments non lus");
+define ('SEE_ONLY_UNREAD', "Seulement afficher les éléments non lus (%d)");
+define ('SEE_ALL', "Voir tous les %d éléments pour ce canal");
+define ('H2_SEARCH_RESULTS_FOR', "%d résultats pour %s");
+
+define ('SEARCH_SEARCH_QUERY','Elements de recherche:');
+define ('SEARCH_EXACT_MATCH', 'Résultat exacte');
+define ('SEARCH_CONTAINS', 'Résultat partiel');
+
+define ('H2_UNREAD_ITEMS',"Elements non lus (%d)");
+define ('H2_RECENT_ITEMS', "Elements récents");
+define ('H2_CHANNELS','Channels');
+define ('ITEMCOUNT_PF', '<strong>%d</strong> éléments (<strong>%d</strong> non lus) dans <strong>%d</strong> canaux');
+define ('UNREAD_PF', '(<strong>%d non lus</strong>)');
+
+define ('FTR_POWERED_BY', "is powered by ");
+
+define ('NAV_HOME','Départ');
+define ('NAV_UPDATE', 'Rafraichir');
+define ('NAV_CHANNEL_ADMIN', 'Admministration');
+define ('NAV_SEARCH', "Chercher");
+
+define ('POSTED', 'Publié: ');
+define ('SEARCH_GO', 'Chercher');
+
+define ('HOME_FOLDER','Racine');
+
+// admin
+define ('ADMIN_EDIT', 'éditer');
+define ('ADMIN_DELETE', 'effacer');
+define ('ADMIN_DELETE2', 'Effacer');
+define ('ADMIN_RENAME', 'Renommer en...');
+define ('ADMIN_CREATE', 'Créer');
+define ('ADMIN_IMPORT','Importer');
+define ('ADMIN_EXPORT','Exporter');
+define ('ADMIN_ADD','Ajouter');
+define ('ADMIN_YES', 'Oui');
+define ('ADMIN_NO', 'Non');
+define ('ADMIN_FOLDERS','Répertoires:');
+define ('ADMIN_CHANNELS','Canaux:');
+define ('ADMIN_OPML','OPML:');
+
+define ('ADMIN_SUBMIT_CHANGES', 'Poster les modifications');
+define ('ADMIN_CHANNELS_HEADING_TITLE','Titre');
+define ('ADMIN_CHANNELS_HEADING_FOLDER','Répertoire');
+define ('ADMIN_CHANNELS_HEADING_DESCR','Description');
+define ('ADMIN_CHANNELS_HEADING_ACTION','Action');
+define ('ADMIN_CHANNELS_ADD','Ajouter un canal:');
+
+define ('ADMIN_OPML_EXPORT','Exporter OPML:');
+define ('ADMIN_OPML_IMPORT','Importer OPML:');
+
+define ('ADMIN_CHANNEL_NAME','Titre:');
+define ('ADMIN_CHANNEL_RSS_URL','URL RSS:');
+define ('ADMIN_CHANNEL_SITE_URL','URL Site:');
+define ('ADMIN_CHANNEL_FOLDER','Dans le répertoire:');
+define ('ADMIN_CHANNEL_DESCR','Description:');
+
+define ('ADMIN_ARE_YOU_SURE', "Êtes-vous sûr de vouloir éffacer '%s'?");
+?>
