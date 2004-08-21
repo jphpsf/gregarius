b59d4e7951f84e1c182cc1b32e068a124567099d
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2004-08-21 11:52:46 +0000

    auto-select of the content of the add channel url on focus
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@182 d826556c-87f9-0310-90b4-9554b5b11594

--- channel_admin.php
+++ channel_admin.php
@@ -89,7 +89,7 @@ function channels() {
     echo "<form method=\"post\" action=\"" .$_SERVER['PHP_SELF'] ."\">\n";
     echo "<p><input type=\"hidden\" name=\"". ADMIN_DOMAIN."\" value=\"".ADMIN_DOMAIN_CHANNEL."\"/>\n";
     echo "<label for=\"new_channel\">". ADMIN_CHANNELS_ADD ."</label>\n";
-    echo "<input type=\"text\" name=\"new_channel\" id=\"new_channel\" value=\"http://\" />\n";
+    echo "<input type=\"text\" name=\"new_channel\" id=\"new_channel\" value=\"http://\" onfocus=\"this.select()\"/>\n";
 
     echo "<label for=\"add_channel_to_folder\">". ADMIN_IN_FOLDER . "</label>\n";
     folder_combo('add_channel_to_folder');
