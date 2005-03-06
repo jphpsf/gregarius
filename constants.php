6660b56ceaa8b536e2a09792c1071725db16cfdc
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-03-06 16:37:40 +0000

    0.3.6
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@307 d826556c-87f9-0310-90b4-9554b5b11594

--- constants.php
+++ constants.php
@@ -33,7 +33,7 @@ define ('_TITLE_', "Gregarius");
 
 
 // Application version
-define ('_VERSION_', "0.3.5");
+define ('_VERSION_', "0.3.6");
 define ('MINUTE',60);
 
 define ('COOKIE_LIFESPAN',60*60*24*999);
