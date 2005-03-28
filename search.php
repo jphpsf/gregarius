b8b491a3503e7691fced6eaa5d82cc9c23db7d30
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-03-28 19:08:16 +0000

    cleanup, formatting
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@328 d826556c-87f9-0310-90b4-9554b5b11594

--- search.php
+++ search.php
@@ -48,10 +48,10 @@ require_once("init.php");
 
 if (array_key_exists(QUERY_PRM,$_REQUEST) && strlen($_REQUEST[QUERY_PRM]) > 1) {
     rss_header("Search",LOCATION_SEARCH);
-    sideChannels(false);  
+    sideChannels(false);
     search();
 } else {
-    rss_header(TITLE_SEARCH,LOCATION_SEARCH,"document.getElementById('query').focus()");  
+    rss_header(TITLE_SEARCH,LOCATION_SEARCH,"document.getElementById('query').focus()");
     sideChannels(false);
     list($cnt) = rss_fetch_row(rss_query('select count(*) from ' . getTable("item")));
     searchForm(sprintf(H2_SEARCH, $cnt));
@@ -60,7 +60,6 @@ if (array_key_exists(QUERY_PRM,$_REQUEST) && strlen($_REQUEST[QUERY_PRM]) > 1) {
 rss_footer();
 
 function searchForm($title) {
-    
 
     echo "\n\n<div id=\"searchfrm\" class=\"frame\">";
 
@@ -71,15 +70,15 @@ function searchForm($title) {
       ."\n\t\t<p><label for=\"query\">". SEARCH_SEARCH_QUERY ."</label><input type=\"text\" name=\"query\" "
       ." id=\"query\" value=\"". (array_key_exists(QUERY_PRM,$_REQUEST)?$_REQUEST[QUERY_PRM]:"")
 	."\"/></p>\n"
-      
-      ."\n\t\t<p><input type=\"radio\" id=\"qry_match_or\" name=\"". QUERY_MATCH_MODE 
+
+      ."\n\t\t<p><input type=\"radio\" id=\"qry_match_or\" name=\"". QUERY_MATCH_MODE
       ."\" value=\"". QUERY_MATCH_OR."\""
-      .((array_key_exists(QUERY_MATCH_MODE,$_REQUEST) && 
+      .((array_key_exists(QUERY_MATCH_MODE,$_REQUEST) &&
 	 $_REQUEST[QUERY_MATCH_MODE] == QUERY_MATCH_OR)?" checked=\"checked\"":"")
 	."/>\n"
       ."\t\t<label for=\"qry_match_or\">". SEARCH_MATCH_OR."</label>\n"
 
-      ."\t\t<input type=\"radio\" id=\"qry_match_and\" name=\"". QUERY_MATCH_MODE 
+      ."\t\t<input type=\"radio\" id=\"qry_match_and\" name=\"". QUERY_MATCH_MODE
       ."\" value=\"". QUERY_MATCH_AND ."\""
       .((array_key_exists(QUERY_MATCH_MODE,$_REQUEST) &&
 	 $_REQUEST[QUERY_MATCH_MODE] == QUERY_MATCH_AND ||
@@ -91,14 +90,13 @@ function searchForm($title) {
       ."\" value=\"". QUERY_MATCH_EXACT ."\""
       .((array_key_exists(QUERY_MATCH_MODE,$_REQUEST) &&
 	 $_REQUEST[QUERY_MATCH_MODE] == QUERY_MATCH_EXACT)?" checked=\"checked\"":"")
-	."/>\n"  
+	."/>\n"
       ."\t\t<label for=\"qry_match_exact\">". SEARCH_MATCH_EXACT."</label></p>\n"
-      
-      
+
       ."\n\t\t<p><label for=\"". QUERY_CHANNEL ."\">". SEARCH_CHANNELS ."</label>\n"
       ."\t\t<select name=\"".QUERY_CHANNEL."\" id=\"".QUERY_CHANNEL."\">\n"
       ."\t\t\t<option value=\"". ALL_CHANNELS_ID ."\""
-      .((!array_key_exists(QUERY_CHANNEL,$_REQUEST) || 
+      .((!array_key_exists(QUERY_CHANNEL,$_REQUEST) ||
 	 $_REQUEST[QUERY_CHANNEL] == ALL_CHANNELS_ID)?" selected=\"selected\"":"")
 	.">" . ALL  . "</option>\n";
 
@@ -108,9 +106,7 @@ function searchForm($title) {
 		      ." where f.id=c.parent "
 		      ." order by c.parent asc,"
 		      .((getConfig('rss.config.absoluteordering'))?"c.position asc":"c.title asc"));
-    
-    
-    
+
     $prev_parent = -1;
     while (list($id_,$title_, $parent_, $parent_id_) = rss_fetch_row($res)) {
         if ($prev_parent != $parent_id_) {
@@ -121,21 +117,20 @@ function searchForm($title) {
             echo "\t\t\t<optgroup label=\"$parent_ /\">\n";
             $prev_parent = $parent_id_;
         }
-        
+
         if (strlen($title_ ) > 25) {
             $title_ = substr($title_,0,22) . "...";
         }
         echo "\t\t\t\t<option value=\"$id_\""
-          .((array_key_exists(QUERY_CHANNEL,$_REQUEST) && 
+          .((array_key_exists(QUERY_CHANNEL,$_REQUEST) &&
              $_REQUEST[QUERY_CHANNEL] == $id_)?" selected=\"selected\"":"")
             .">$title_</option>\n";
     }
 
-
     if ($prev_parent != 0) {
         echo "\t\t\t</optgroup>\n";
     }
-    
+
     echo "\t\t</select></p>\n";
 
     echo "\n\t\t<p><input type=\"radio\" id=\"qry_order_date\" name=\"". QUERY_ORDER_BY
@@ -144,299 +139,289 @@ function searchForm($title) {
 	 $_REQUEST[QUERY_ORDER_BY] == QUERY_ORDER_BY_DATE ||
 	 !array_key_exists(QUERY_ORDER_BY,$_REQUEST)?" checked=\"checked\"":""))
 	."/>\n"
-      ."\t\t<label for=\"qry_order_date\">". SEARCH_ORDER_DATE_CHANNEL ."</label>\n"      
+      ."\t\t<label for=\"qry_order_date\">". SEARCH_ORDER_DATE_CHANNEL ."</label>\n"
       ."\t\t<input type=\"radio\" id=\"qry_order_channel\" name=\"". QUERY_ORDER_BY
       ."\" value=\"". QUERY_ORDER_BY_CHANNEL ."\""
       .((array_key_exists(QUERY_ORDER_BY,$_REQUEST) &&
 	 $_REQUEST[QUERY_ORDER_BY] == QUERY_ORDER_BY_CHANNEL)?" checked=\"checked\"":"")
 	."/>\n"
       ."\t\t<label for=\"qry_order_channel\">". SEARCH_ORDER_CHANNEL_DATE ."</label></p>\n";
-    
-    
-    
+
     echo "\n\t\t<p><label for=\"". QUERY_RESULTS ."\">". SEARCH_RESULTS_PER_PAGE ."</label>\n"
       ."\t\t<select name=\"".QUERY_RESULTS."\" id=\"".QUERY_RESULTS."\">\n"
 
       ."\t\t\t<option value=\"5\""
       .((array_key_exists(QUERY_RESULTS,$_REQUEST) && $_REQUEST[QUERY_RESULTS] == 5?" selected=\"selected\"":""))
 	.">5</option>\n"
-      
+
       ."\t\t\t<option value=\"15\""
       .((
-	(array_key_exists(QUERY_RESULTS,$_REQUEST) && $_REQUEST[QUERY_RESULTS] == 15)
-	|| !array_key_exists(QUERY_RESULTS,$_REQUEST)
-	)?" selected=\"selected\"":"")
+	 (array_key_exists(QUERY_RESULTS,$_REQUEST) && $_REQUEST[QUERY_RESULTS] == 15)
+	 || !array_key_exists(QUERY_RESULTS,$_REQUEST)
+	 )?" selected=\"selected\"":"")
 	.">15</option>\n"
-      
-      
 
-    ."\t\t\t<option value=\"50\""
+      ."\t\t\t<option value=\"50\""
       .((array_key_exists(QUERY_RESULTS,$_REQUEST) && $_REQUEST[QUERY_RESULTS] == 50?" selected=\"selected\"":""))
 	.">50</option>\n"
-    
-    ."\t\t\t<option value=\"".INFINE_RESULTS."\""
+
+      ."\t\t\t<option value=\"".INFINE_RESULTS."\""
       .((array_key_exists(QUERY_RESULTS,$_REQUEST) && $_REQUEST[QUERY_RESULTS] == INFINE_RESULTS?" selected=\"selected\"":""))
 	.">".ALL."</option>\n"
-                                
-    
+
       ."\t\t</select></p>\n"
 
       ."\n\t\t<p><input type=\"hidden\" name=\"".QUERY_CURRENT_PAGE."\" value=\"0\" />\n"
       ."<input id=\"search_go\" type=\"submit\" value=\"". SEARCH_GO ."\"/></p>\n"
       ."\t\t</form>\n";
-    
 
-    echo "</div>\n";    
+    echo "</div>\n";
 }
 
 function search() {
 
-    
     $qry = $_REQUEST[QUERY_PRM];
     $matchMode = (!array_key_exists(QUERY_MATCH_MODE, $_REQUEST)?QUERY_MATCH_AND:$_REQUEST[QUERY_MATCH_MODE]);
     $channelId = (array_key_exists(QUERY_CHANNEL,$_REQUEST))?(int)$_REQUEST[QUERY_CHANNEL]:ALL_CHANNELS_ID;
-    $resultsPerPage = (array_key_exists(QUERY_RESULTS,$_REQUEST))?(int)$_REQUEST[QUERY_RESULTS]:INFINE_RESULTS;   
+    $resultsPerPage = (array_key_exists(QUERY_RESULTS,$_REQUEST))?(int)$_REQUEST[QUERY_RESULTS]:INFINE_RESULTS;
     $currentPage = (array_key_exists(QUERY_CURRENT_PAGE,$_REQUEST)?(int)$_REQUEST[QUERY_CURRENT_PAGE]:0);
     $orderBy = (array_key_exists(QUERY_ORDER_BY, $_REQUEST)?$_REQUEST[QUERY_ORDER_BY]:QUERY_ORDER_BY_DATE);
     $qWhere = "";
     $regMatch = "";
     $term = "";
     if ($matchMode == QUERY_MATCH_OR || $matchMode == QUERY_MATCH_AND) {
+
+		$logicSep = ($matchMode == QUERY_MATCH_OR?"or":"and");
+		$terms = explode(" ",$qry);
+		foreach($terms as $term) {
+			 $term = trim($term);
+			 if ($term != "") {
+			$qWhere .=
+			  "(i.description like '%$term%' or "
+			  ." i.title like '%$term%') " .$logicSep;
+			 }
+			 // this will be used later for the highliting regexp
+			 if ($regMatch != "") {
+			$regMatch .= "|";
+			 }
+			 $regMatch .= $term;
+		}
 	
-	$logicSep = ($matchMode == QUERY_MATCH_OR?"or":"and");
-	$terms = explode(" ",$qry);    
-	foreach($terms as $term) {
-	    $term = trim($term);
-	    if ($term != "") {	    
-		$qWhere .= 
+		$qWhere .= ($matchMode == QUERY_MATCH_OR?" 1=0 ":" 1=1 ");
+	} else {
+		$logicSep = "";
+		$terms[0] = $qry;
+		$qWhere .=
 		  "(i.description like '%$term%' or "
-		  ." i.title like '%$term%') " .$logicSep;
-	    }
-	    // this will be used later for the highliting regexp
-	    if ($regMatch != "") {
-		$regMatch .= "|";
-	    }
-	    $regMatch .= $term;
+		  ." i.title like '%$term%') ";
+		$regMatch = $qry;
 	}
-	
-	$qWhere .= ($matchMode == QUERY_MATCH_OR?" 1=0 ":" 1=1 ");
-    } else {
-	$logicSep = "";
-	$terms[0] = $qry;
-	$qWhere .= 
-	  "(i.description like '%$term%' or "
-	  ." i.title like '%$term%') ";
-	$regMatch = $qry;
-    }
-    
-    $sql = "select distinct i.title, c.title, c.id, i.unread, i.url, "
+
+   $sql = "select distinct i.title, c.title, c.id, i.unread, i.url, "
       ." i.description, c.icon, "
       ." if (i.pubdate is null, unix_timestamp(i.added), unix_timestamp(i.pubdate)) as ts, "
-      ." i.pubdate is not null as ispubdate, " 
+      ." i.pubdate is not null as ispubdate, "
       ." i.id  "
       ." from " . getTable("item") ." i, " . getTable("channels") ." c, " . getTable("folders") ." f "
       ." where i.cid=c.id and c.parent=f.id and ("
       .$qWhere .") ";
 
-    if ($channelId != ALL_CHANNELS_ID) {
-	$sql .= " and c.id = $channelId ";
-    }
-    
-    if ($orderBy == QUERY_ORDER_BY_DATE) {
-	$sql .= " order by 8 desc";
-    } else {
-	if (getConfig('rss.config.absoluteordering')) {
-	    $sql .= " order by f.position asc, c.position asc";
-	} else {
-	    $sql .= " order by c.parent asc, c.title asc";
-	}
-    }
-    
+   if ($channelId != ALL_CHANNELS_ID) {
+		$sql .= " and c.id = $channelId ";
+   }
 
-    $sql .=", i.added desc";
-    
-    //echo $sql;
-    
-    $res0=rss_query($sql);
-    $cnt = rss_num_rows($res0);
-    $items = array();
+   if ($orderBy == QUERY_ORDER_BY_DATE) {
+		$sql .= " order by 8 desc";
+   } else {
+		if (getConfig('rss.config.absoluteordering')) {
+	   	$sql .= " order by f.position asc, c.position asc";
+		} else {
+	    	$sql .= " order by c.parent asc, c.title asc";
+		}
+   }
 
-                
-    $start__ = $resultsPerPage * $currentPage;
-    $end__  = $start__ + $resultsPerPage -1;
-    if ($resultsPerPage == INFINE_RESULTS) {       
-	$end__ = 99999999;
-    }
-      
-    if ($cnt > 0) {
+   $sql .=", i.added desc";
 
-	$crnt = 0;
+   //echo $sql;
+
+   $res0=rss_query($sql);
+   $cnt = rss_num_rows($res0);
+   $items = array();
+
+   $start__ = $resultsPerPage * $currentPage;
+   $end__  = $start__ + $resultsPerPage -1;
+   if ($resultsPerPage == INFINE_RESULTS) {
+		$end__ = 99999999;
+   }
+
+	if ($cnt > 0) {
 	
-	$items=array();
-        while (list($ititle,$ctitle, $cid, $iunread, $iurl, $idescr,  $cicon, $its, $iispubdate, $iid) = rss_fetch_row($res0)) {
-
-	    $descr_noTags = strip_tags($idescr);
-	    $title_noTags = strip_tags($ititle);
-	    
-	    
-	    $match = false;
-	    reset($terms);
-	    $match = ($matchMode == QUERY_MATCH_AND || $matchMode == QUERY_MATCH_EXACT);
-	    foreach ($terms as $term) {
-		if ($matchMode == QUERY_MATCH_AND || $matchMode == QUERY_MATCH_EXACT) {		   
-		    $match =  ((stristr($descr_noTags,$term) || stristr($title_noTags, $term)) && $match);
-		} else {
-		    $match = ($match || (stristr($descr_noTags,$term) || stristr($title_noTags, $term)));
-		}
-		    
-	    }
-	    
-	    if ($match) {				
-		if (isset($hits) && is_array($hits) && array_key_exists($iid,$hits) && $hits[$iid]) {
-		    continue;
-		}
-		
-		
-		if ($crnt >= $start__ && $crnt <= $end__) {
-		    $hits[$iid] = true;		
-		    $items[]=
-		      array($cid,$ctitle,$cicon,
-			    
-			    // Credits for the regexp: mike at iaym dot com
-			    // http://ch2.php.net/manual/en/function.preg-replace.php
-			    "$ititle",
-			    $iunread,
-			    $iurl,
-			    preg_replace("'(?!<.*?)($regMatch)(?![^<>]*?>)'si",
-					 HIT_BEFORE . "\\1" . HIT_AFTER,
-					 $idescr),
-			    $its,
-			    $iispubdate,
-			    $iid
-			    );
-		    
-		} 
-		
-		
-		$crnt++;
-	    }	    
-        }
-    }
-    
-    $cnt = $crnt;
-
-    $humanReadableQuery = implode(" ".strtoupper($logicSep)." ",$terms);
-    
-    $title = sprintf(
-		     (($cnt > 1 || $cnt == 0)?H2_SEARCH_RESULTS_FOR:H2_SEARCH_RESULT_FOR), 
-		     $cnt, "'" .$humanReadableQuery."'");
-
-    // If we got no hit, offer the search form.
-    if ($cnt > 0) {
-	searchForm($title,false);
+		$crnt = 0;
 	
-	$nav = "";
-	if ($resultsPerPage != INFINE_RESULTS && $cnt > $resultsPerPage) {
-	    $nav .= "<div class=\"readmore\">";
-	    $nav .= SEARCH_RESULTS;
-	    
-	    
-	    // first page
-	    $fp = 0;
-	    //last page
-	    $lp = floor(($cnt -1) / $resultsPerPage);
-	    // current page
-	    $cp = $currentPage;
-	    //shown pages
-	    $pages = array();
-
-	    for ($i=0;$i<4;$i++) {
-		if ($cp - $i >= 0) {
-		    $pages[$cp - $i] = true;
-		} else {
-		    if ($cp + $i < $lp) {
-			$pages[$cp + $i] = true;
-		    }
-		}
-		if ($cp + $i < $lp) {
-		    $pages[$cp + $i] = true;
-		} else {
-		    if ($cp - $i >= 0) {
-			$pages[$cp - $i] = true;
-		    }
-		}
-	    }
-	    
-	    $pages[0] = true;
-	    $pages[$lp] = true;
-	    
-	    
-	    for ($p=$fp;$p<$lp;$p++) {
-		if (!array_key_exists($p,$pages)) {
-		    if (array_key_exists($p - 1 ,$pages)) {
-			$nav .= " ... ";
-		    } 
-		    continue;
-		}
+		$items=array();
+		while (list($ititle,$ctitle, $cid, $iunread, $iurl, $idescr,  $cicon, $its, $iispubdate, $iid) = rss_fetch_row($res0)) {
+			$descr_noTags = strip_tags($idescr);
+			$title_noTags = strip_tags($ititle);
 		
-		$cpp = ($p * $resultsPerPage == $start__);
-		if (!$cpp) {
-		    $nav .= " <a href=\""
-		      .$_SERVER['PHP_SELF'] . "?"
-		      .QUERY_PRM . "=$qry&amp;"
-		      .QUERY_MATCH_MODE . "=$matchMode&amp;"
-		      .QUERY_CHANNEL . "=$channelId&amp;"
-		      .QUERY_RESULTS . "=$resultsPerPage&amp;"
-		      .QUERY_ORDER_BY . "=$orderBy&amp;"
-		      .QUERY_CURRENT_PAGE ."=$p"
-		      ."\">";
-		} else {
-		    $nav .= HIT_BEFORE;
-		}
+			$match = false;
+			reset($terms);
+			$match = ($matchMode == QUERY_MATCH_AND || $matchMode == QUERY_MATCH_EXACT);
+			foreach ($terms as $term) {
+				if ($matchMode == QUERY_MATCH_AND || $matchMode == QUERY_MATCH_EXACT) {
+					 $match =  ((stristr($descr_noTags,$term) || stristr($title_noTags, $term)) && $match);
+				} else {
+					 $match = ($match || (stristr($descr_noTags,$term) || stristr($title_noTags, $term)));
+				}
 		
-		$nav .= "". (1+$p * $resultsPerPage) ."-" . ((1+$p) * $resultsPerPage) ."";
+			}
 		
-		if (!$cpp) {
-		    $nav .= "</a>";
-		} else {
-		    $nav .= HIT_AFTER;
+			if ($match) {
+				if (isset($hits) && is_array($hits) && array_key_exists($iid,$hits) && $hits[$iid]) {
+					 continue;
+				}
+		
+				if ($crnt >= $start__ && $crnt <= $end__) {
+					$hits[$iid] = true;
+					$items[]= array(
+						$cid,
+						$ctitle,
+						$cicon,
+						// Credits for the regexp: mike at iaym dot com
+						// http://ch2.php.net/manual/en/function.preg-replace.php
+						"$ititle",
+						$iunread,
+						$iurl,
+						preg_replace(
+							"'(?!<.*?)($regMatch)(?![^<>]*?>)'si",
+							HIT_BEFORE . "\\1" . HIT_AFTER,
+							$idescr
+						),
+						$its,
+						$iispubdate,
+						$iid
+					);
+		
+				}
+		
+				$crnt++;
+			}
 		}
-		if ((1+$p) * $resultsPerPage < $cnt) {
-		    $nav .= ", \n";
-		}
-	    }
-	    
-	    if ($p * $resultsPerPage >= $end__) {
-		$nav .= " <a href=\""
-		  .$_SERVER['PHP_SELF'] . "?"
-		  .QUERY_PRM . "=$qry&amp;"
-		  .QUERY_MATCH_MODE . "=$matchMode&amp;"
-		  .QUERY_CHANNEL . "=$channelId&amp;"
-		  .QUERY_RESULTS . "=$resultsPerPage&amp;"
-		  .QUERY_ORDER_BY . "=$orderBy&amp;"
-		  .QUERY_CURRENT_PAGE ."=$p"
-		  ."\">";
-		$nav .=  (1+ $p * $resultsPerPage) ."-$cnt";
-		$nav .= "</a> \n";
-	    } else {
-		$nav .= HIT_BEFORE . (1+ $p * $resultsPerPage) ."-$cnt" .HIT_AFTER;
-	    }
-	    $nav .= "<hr class=\"clearer hidden\"/>\n</div>\n";
 	}
+
+   $cnt = $crnt;
+
+   $humanReadableQuery = implode(" ".strtoupper($logicSep)." ",$terms);
+
+   $title = sprintf(
+		(($cnt > 1 || $cnt == 0)?H2_SEARCH_RESULTS_FOR:H2_SEARCH_RESULT_FOR),
+		$cnt, "'" .$humanReadableQuery."'");
+
+   // If we got no hit, offer the search form.
+   if ($cnt > 0) {
+		searchForm($title,false);
+
+		$nav = "";
+		if ($resultsPerPage != INFINE_RESULTS && $cnt > $resultsPerPage) {
+	    	$nav .= "<div class=\"readmore\">";
+	    	$nav .= SEARCH_RESULTS;
+
+	    	// first page
+	    	$fp = 0;
+	    	//last page
+	    	$lp = floor(($cnt -1) / $resultsPerPage);
+	    	// current page
+	    	$cp = $currentPage;
+	    	//shown pages
+	    	$pages = array();
+
+	    	for ($i=0;$i<4;$i++) {
+				if ($cp - $i >= 0) {
+		    		$pages[$cp - $i] = true;
+				} else {
+		    		if ($cp + $i < $lp) {
+						$pages[$cp + $i] = true;
+		    		}
+				}
+				if ($cp + $i < $lp) {
+		    		$pages[$cp + $i] = true;
+				} else {
+		    		if ($cp - $i >= 0) {
+						$pages[$cp - $i] = true;
+		    		}
+				}
+	      }
+
+	    	$pages[0] = true;
+	    	$pages[$lp] = true;
+
+	    	for ($p=$fp;$p<$lp;$p++) {
+				if (!array_key_exists($p,$pages)) {
+		    		if (array_key_exists($p - 1 ,$pages)) {
+						$nav .= " ... ";
+		    		}
+		    		continue;
+				}
+
+				$cpp = ($p * $resultsPerPage == $start__);
+				if (!$cpp) {
+		    		$nav .= " <a href=\""
+		      		.$_SERVER['PHP_SELF'] . "?"
+						.QUERY_PRM . "=$qry&amp;"
+						.QUERY_MATCH_MODE . "=$matchMode&amp;"
+						.QUERY_CHANNEL . "=$channelId&amp;"
+						.QUERY_RESULTS . "=$resultsPerPage&amp;"
+						.QUERY_ORDER_BY . "=$orderBy&amp;"
+						.QUERY_CURRENT_PAGE ."=$p"
+						."\">";
+				} else {
+		    		$nav .= HIT_BEFORE;
+				}
+
+				$nav .= "". (1+$p * $resultsPerPage) ."-" . ((1+$p) * $resultsPerPage) ."";
+
+				if (!$cpp) {
+		    		$nav .= "</a>";
+				} else {
+		    		$nav .= HIT_AFTER;
+				}
+				if ((1+$p) * $resultsPerPage < $cnt) {
+		    		$nav .= ", \n";
+				}
+	      }
+
+	    	if ($p * $resultsPerPage >= $end__) {
+				$nav .= " <a href=\""
+			  .$_SERVER['PHP_SELF'] . "?"
+			  .QUERY_PRM . "=$qry&amp;"
+			  .QUERY_MATCH_MODE . "=$matchMode&amp;"
+			  .QUERY_CHANNEL . "=$channelId&amp;"
+			  .QUERY_RESULTS . "=$resultsPerPage&amp;"
+			  .QUERY_ORDER_BY . "=$orderBy&amp;"
+			  .QUERY_CURRENT_PAGE ."=$p"
+			  ."\">";
+				$nav .=  (1+ $p * $resultsPerPage) ."-$cnt";
+				$nav .= "</a> \n";
+			} else {
+				$nav .= HIT_BEFORE . (1+ $p * $resultsPerPage) ."-$cnt" .HIT_AFTER;
+	    	}
+	    	$nav .= "<hr class=\"clearer hidden\"/>\n</div>\n";
+		}
+
+		echo "<div id=\"items\" class=\"frame\">";
+		if ($nav) {
+	    	echo $nav;
+		}
+		
+		itemsList( "", $items, IL_NO_COLLAPSE);
 	
-	echo "<div id=\"items\" class=\"frame\">";
-	if ($nav) {
-	    echo $nav;
-	}
-	itemsList( "", $items, IL_NO_COLLAPSE);
-	if ($nav) {
-	    echo $nav;
-	}
-	echo "</div>\n";
-    } else {
-	searchForm($title,true);
-    }
-
+		if ($nav) {
+	    	echo $nav;
+		}
+	
+		echo "</div>\n";
+   } else {
+		searchForm($title,true);
+   }
 }
 
 ?>
