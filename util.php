0858e7d49be9a1b3cd0b6a8baaea09dd06795cd7
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-04-04 12:40:23 +0000

    temporary fix to google indexing invalid fcollapse urls
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@368 d826556c-87f9-0310-90b4-9554b5b11594

--- util.php
+++ util.php
@@ -50,14 +50,16 @@ function rss_header($title="", $active=0, $onLoadAction="", $no_output_buffering
 	  ."\" />\n"
 	  ."\t<title>".makeTitle($title)."</title>\n";
 
-	if (getConfig('rss.config.robotsmeta')) {
-	  $meta = (
-		  (
-		   array_key_exists('expand',$_REQUEST) || 
-		   array_key_exists('collapse',$_REQUEST) ||
-		   array_key_exists('fcollapse',$_REQUEST) ||
-		   array_key_exists('fexpand',$_REQUEST) ||
-		   array_key_exists('dbg',$_REQUEST)
+    if (getConfig('rss.config.robotsmeta')) {
+	$meta = (
+		 (
+		  array_key_exists('expand',$_REQUEST) || 
+		  array_key_exists('collapse',$_REQUEST) ||		  
+		  strstr($_SERVER['REQUEST_URI'], 'fcollapse') ||
+		  strstr($_SERVER['REQUEST_URI'],'fexpand') ||
+		  //array_key_exists('fcollapse',$_REQUEST) ||
+		  //array_key_exists('fexpand',$_REQUEST) ||
+		  array_key_exists('dbg',$_REQUEST)
 		 )?'noindex,follow':getConfig('rss.config.robotsmeta'));
 	  echo "\t<meta name=\"robots\" content=\"$meta\"/>\n";
 	}
