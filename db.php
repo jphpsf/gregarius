a6a190495707c15526e9502579960fd342e7495d
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-05-02 14:13:26 +0000

    fall back to mysql_escape_string when mysql_real_escape_string isn't compiled in
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@418 d826556c-87f9-0310-90b4-9554b5b11594

--- db.php
+++ db.php
@@ -108,7 +108,13 @@ function rss_insert_id() {
 }
 
 function rss_real_escape_string($string) {
-    return mysql_real_escape_string($string);
+    if (function_exists('mysql_real_escape_string')) {
+	return mysql_real_escape_string($string);
+    } elseif (function_exists('mysql_escape_string')) {
+	return mysql_escape_string($string);
+    } else {
+	die( rss_error("Your PHP version doesn't meet Gregarius' minimal requirements, please consider upgrading!", true));
+    }
 }
 
 function getTable($tableName) {
