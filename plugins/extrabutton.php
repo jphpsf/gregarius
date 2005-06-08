9a2d1ebcba9bca18c9c4ccf1d617797edb89e23b
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-06-08 11:22:14 +0000

    removed white space after closing ?>
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@498 d826556c-87f9-0310-90b4-9554b5b11594

--- plugins/extrabutton.php
+++ plugins/extrabutton.php
@@ -1,59 +1,117 @@
 <?
+
 ###############################################################################
+
 # Gregarius - A PHP based RSS aggregator.
+
 # Copyright (C) 2003 - 2005 Marco Bonetti
+
 #
+
 ###############################################################################
+
 # File: $Id$ $Name$
+
 #
+
 ###############################################################################
+
 # This program is free software and open source software; you can redistribute
+
 # it and/or modify it under the terms of the GNU General Public License as
+
 # published by the Free Software Foundation; either version 2 of the License,
+
 # or (at your option) any later version.
+
 #
+
 # This program is distributed in the hope that it will be useful, but WITHOUT
+
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+
 # more details.
+
 #
+
 # You should have received a copy of the GNU General Public License along
+
 # with this program; if not, write to the Free Software Foundation, Inc.,
+
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
+
 # http://www.gnu.org/licenses/gpl.html
+
 #
+
 ###############################################################################
+
 # E-mail:      mbonetti at users dot sourceforge dot net
+
 # Web page:    http://sourceforge.net/projects/gregarius
+
 #
+
 ###############################################################################
+
 # $Log$
+# Revision 1.2  2005/06/08 11:22:14  mbonetti
+# removed white space after closing ?>
+#
 # Revision 1.1  2005/05/25 19:02:43  mbonetti
 # Adds an extra "Mark Feed As Read" button at the bottom of each feed view
 #
+
 # Revision 1.7  2005/05/20 07:42:21  mbonetti
+
 # CVS Log messages in the file header
+
 #
+
 #
+
 ###############################################################################
 
 
+
+
+
 /// Name: Extra Button
+
 /// Author: Marco Bonetti
+
 /// Description: Adds an extra "Mark Feed As Read" button at the bottom of each feed view
+
 /// Version: $Revision$
 
 
+
+
+
 function __extra_button_Button($in) {
+
     list($feedCount,$unreadCount,$readCount,$itemId,$channelId) = $in;
+
     if ($unreadCount && $itemId == "" && $feedCount==1) {
+
         echo "<span style=\"text-align:right\">\n";
+
 		markReadForm($channelId);
+
 		echo "</span>\n";
+
 	}
+
 	return null;
+
 }
+
 	
+
 rss_set_hook('rss.plugins.items.afteritems','__extra_button_Button');
+
+
 
 ?>
