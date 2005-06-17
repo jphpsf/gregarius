42026eba2e8fc8f647423dcfc6fb715611394d67
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-06-17 18:11:57 +0000

    Marks an item as read when you doubleclick it
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@532 d826556c-87f9-0310-90b4-9554b5b11594

---
+++ plugins/dblclicktoread.php
@@ -0,0 +1,72 @@
+<?
+###############################################################################
+# Gregarius - A PHP based RSS aggregator.
+# Copyright (C) 2003 - 2005 Marco Bonetti
+#
+###############################################################################
+# File: $Id: extrabutton.php 511 2005-06-14 08:48:03Z mbonetti $ $Name$
+#
+###############################################################################
+# This program is free software and open source software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
+# http://www.gnu.org/licenses/gpl.html
+#
+###############################################################################
+# E-mail:      mbonetti at users dot sourceforge dot net
+# Web page:    http://sourceforge.net/projects/gregarius
+#
+###############################################################################
+
+
+/// Name: Doubleclick to Read
+/// Author: Marco Bonetti
+/// Description: Marks an item as read when you doubleclick it
+/// Version: 1.0
+
+function __dblclickToRead_js($in) {
+?>
+<script type="text/javascript">
+// <!--
+	function __dblclickToRead_js_getId(o) {
+		if (html = o.innerHTML) {
+		//onclick="_es(25026,1);"
+			if (r1 = new RegExp(".*es.([0-9]+),([0-9]+).*","gm").exec(html)) {
+				id=r1[1];
+				s =r1[2] & <?= SET_MODE_READ_STATE ?>;
+				if ((sel = document.getElementById('<?= SHOW_WHAT ?>')) &&
+				    sel.options[sel.selectedIndex].value == <?= SHOW_UNREAD_ONLY ?>) {
+                setItemHide(id);
+				} else{ 
+				    setItemClass(id, 'item even');				     
+				}
+				setState(id,s);
+			} 
+		}
+	}
+	var items = document.getElementsByTagName('li');
+	for (var i=0; i<items.length; i++) {
+		var item = items[i];
+		if (item.className == "item unread") {
+			item.setAttribute("ondblclick","__dblclickToRead_js_getId(this); return false;");
+		} 
+	}
+	
+// -->
+</script>
+<?
+	return null;
+}
+
+rss_set_hook('rss.plugins.bodyend','__dblclickToRead_js');
+?>
