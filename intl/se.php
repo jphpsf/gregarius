8482a9a9ab89d661fbf761eeb986128e69cf45f3
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-06-24 07:59:16 +0000

    corrected labels for Swedish translation
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@570 d826556c-87f9-0310-90b4-9554b5b11594

--- intl/se.php
+++ intl/se.php
@@ -41,16 +41,16 @@ define ('LBL_SEARCH_SEARCH_QUERY','S&ouml;kord:');
 define ('LBL_SEARCH_MATCH_OR', 'N&aring;got av orden (OR)');
 define ('LBL_SEARCH_MATCH_AND', 'Alla orden (AND)');                                                                 
 define ('LBL_SEARCH_MATCH_EXACT', 'Den exakta frasen');
-define ('LBL_SEARCH_CHANNELS', 'Syndikeringsfl&ouml;de:');
-define ('LBL_SEARCH_ORDER_DATE_CHANNEL','Ordna efter datum, syndikeringsfl&ouml;de');
-define ('LBL_SEARCH_ORDER_CHANNEL_DATE','Ordna efter syndikeringsfl&ouml;de, datum');
+define ('LBL_SEARCH_CHANNELS', 'Fl&ouml;de:');
+define ('LBL_SEARCH_ORDER_DATE_CHANNEL','Ordna efter datum, fl&ouml;de');
+define ('LBL_SEARCH_ORDER_CHANNEL_DATE','Ordna efter fl&ouml;de, datum');
 define ('LBL_SEARCH_RESULTS_PER_PAGE','Resultat per sida:');
 define ('LBL_SEARCH_RESULTS','Resultat: ');
 define ('LBL_H2_UNREAD_ITEMS',"Ol&auml;sta inl&auml;gg (%d)");
 define ('LBL_H2_RECENT_ITEMS', "Senast l&auml;sta inl&auml;gg");
-define ('LBL_H2_CHANNELS','Syndikeringsfl&ouml;den');
+define ('LBL_H2_CHANNELS','Fl&ouml;den');
 define ('LBL_H5_READ_UNREAD_STATS','%d inl&auml;gg, %d ol&auml;sta');
-define ('LBL_ITEMCOUNT_PF', '<strong>%d</strong> inl&auml;gg (<strong>%d</strong> ol&auml;sta) i <strong>%d</strong> syndikeringsfl&ouml;den');
+define ('LBL_ITEMCOUNT_PF', '<strong>%d</strong> inl&auml;gg (<strong>%d</strong> ol&auml;sta) i <strong>%d</strong> fl&ouml;den');
 define ('LBL_UNREAD_PF', '<strong id="%s" style="%s">(%d ol&auml;sta)</strong>');
 define ('LBL_UNREAD','ol&auml;sta');
 
@@ -79,14 +79,14 @@ define ('LBL_COLLAPSE','[-] Kollapsa');
 define ('LBL_EXPAND','[+] Expandera');
 define ('LBL_PL_FOR','Permalink till ');
 
-define ('LBL_UPDATE_CHANNEL','Syndikeringsfl&ouml;de');
+define ('LBL_UPDATE_CHANNEL','Fl&ouml;de');
 define ('LBL_UPDATE_STATUS','Status');
 define ('LBL_UPDATE_UNDREAD','New Items');
 
 define ('LBL_UPDATE_STATUS_OK','OK (HTTP 200)');
 define ('LBL_UPDATE_STATUS_CACHED', 'OK (Lokal cache)');
 define ('LBL_UPDATE_STATUS_ERROR','ERROR');
-define ('LBL_UPDATE_H2','Uppdaterar %d syndikeringsfl&ouml;den...');
+define ('LBL_UPDATE_H2','Uppdaterar %d fl&ouml;den...');
 define ('LBL_UPDATE_CACHE_TIMEOUT','HTTP Timeout (Local cache)');
 define ('LBL_UPDATE_NOT_MODIFIED','OK (304 Not modified)');
 define ('LBL_UPDATE_NOT_FOUND','404 Not Found (Lokal cache)');
@@ -103,14 +103,13 @@ define ('LBL_ADMIN_ADD','L&auml;gg till');
 define ('LBL_ADMIN_YES', 'Ja');
 define ('LBL_ADMIN_NO', 'Nej');
 define ('LBL_ADMIN_FOLDERS','Foldrar:');
-define ('LBL_ADMIN_CHANNELS','Syndikeringsfl&ouml;den:');
+define ('LBL_ADMIN_CHANNELS','Fl&ouml;den:');
 define ('LBL_ADMIN_OPML','OPML:');  
 define ('LBL_ADMIN_ITEM','Inl&auml;gg:');
 define ('LBL_ADMIN_CONFIG','Konfiguration:');
 define ('LBL_ADMIN_OK','OK');
 define ('LBL_ADMIN_CANCEL','Avbryt');
-//FIXME
-define ('LBL_ADMIN_LOGOUT','Log ud');
+define ('LBL_ADMIN_LOGOUT','Logga ut');
 
 
 define ('LBL_ADMIN_OPML_IMPORT','Importera');
@@ -130,15 +129,15 @@ define ('LBL_ADMIN_CHANNELS_HEADING_DESCR','Beskrivning');
 define ('LBL_ADMIN_CHANNELS_HEADING_MOVE','Ordning');
 define ('LBL_ADMIN_CHANNELS_HEADING_ACTION','');
 define ('LBL_ADMIN_CHANNELS_HEADING_FLAGS','Flaggor');
-define ('LBL_ADMIN_CHANNELS_HEADING_KEY','Key');
-define ('LBL_ADMIN_CHANNELS_HEADING_VALUE','Value');
-define ('LBL_ADMIN_CHANNELS_ADD','L&auml;gg till ett syndikeringsfl&ouml;de:');
+define ('LBL_ADMIN_CHANNELS_HEADING_KEY','Nyckel');
+define ('LBL_ADMIN_CHANNELS_HEADING_VALUE','V&auml;rde');
+define ('LBL_ADMIN_CHANNELS_ADD','L&auml;gg till ett fl&ouml;de:');
 define ('LBL_ADMIN_FOLDERS_ADD','Skapa en folder:');
 define ('LBL_ADMIN_CHANNEL_ICON','Visa favicon:');
 define ('LBL_CLEAR_FOR_NONE','(L&auml;mna tom f&ouml;r att inte visa n&aring;n ikon)');
 
 
-define ('LBL_ADMIN_CONFIG_VALUE','Value');
+define ('LBL_ADMIN_CONFIG_VALUE','V&auml;rde');
 
 define ('LBL_ADMIN_OPML_FILE_IMPORT','Importera lokal OPML-fil:');
 define ('LBL_ADMIN_FILE_IMPORT','Importera');
@@ -152,16 +151,16 @@ define ('LBL_ADMIN_PLUGINS_HEADING_ACTION','Aktiv');
 
 
 
-define ('LBL_ADMIN_CHANNEL_EDIT_CHANNEL','Redigera syndikeringsfl&ouml;de ');
+define ('LBL_ADMIN_CHANNEL_EDIT_CHANNEL','Redigera fl&ouml;de ');
 define ('LBL_ADMIN_CHANNEL_NAME','Rubrik:');
 define ('LBL_ADMIN_CHANNEL_RSS_URL','RSS URL:');
 define ('LBL_ADMIN_CHANNEL_SITE_URL','Sidans URL:');
 define ('LBL_ADMIN_CHANNEL_FOLDER','I foldern:');
 define ('LBL_ADMIN_CHANNEL_DESCR','Beskrivning:');
 define ('LBL_ADMIN_FOLDER_NAME','Foldernamn:');
-define ('LBL_ADMIN_CHANNEL_PRIVATE','Detta syndikeringsfl&ouml;de &auml;r <strong>privat</strong> och kan endast ses av admin.');
-define ('LBL_ADMIN_CHANNEL_DELETED','Detta syndikeringsfl&ouml;de &auml;r <strong>ur bruk</strong>. Den kommer inte 
-uppdateras, och visas inte l&auml;ngre i listan &ouml;ver syndikeringsfl&ouml;den.');
+define ('LBL_ADMIN_CHANNEL_PRIVATE','Detta fl&ouml;de &auml;r <strong>privat</strong> och kan endast ses av admin.');
+define ('LBL_ADMIN_CHANNEL_DELETED','Detta fl&ouml;de &auml;r <strong>ur bruk</strong>. Den kommer inte 
+uppdateras, och visas inte l&auml;ngre i listan &ouml;ver fl&ouml;den.');
 
 define ('LBL_ADMIN_ARE_YOU_SURE', "&Auml;r du s&auml;ker p&aring; att du vill ta bort '%s'?");
 define ('LBL_ADMIN_ARE_YOU_SURE_DEFAULT','&Auml;r du s&auml;ker p&aring; att du vill &aring;terst&auml;lla %s till standardv&auml;rdet \'%s\'?');
