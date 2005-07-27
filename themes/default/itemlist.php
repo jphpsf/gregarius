49b31c859e6ff37d4398645d4f1f3515fef941d5
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-07-27 12:03:21 +0000

    fix for #75. In fact I like the title above the form better
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@670 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/itemlist.php
+++ themes/default/itemlist.php
@@ -1,9 +1,3 @@
-<?php if (rss_itemlist_has_extractions()) { ?>
-<div id="feedaction" class="withmargin">
-	<?php rss_itemlist_prerender_callback() ?>		
-</div>
-<?php } ?>
-
 <?php if ($title = rss_itemlist_title()) { ?>
 <h2<?= rss_itemlist_anchor(); ?>>
 <?php if ($icon = rss_itemlist_icon()) { ?>
@@ -14,7 +8,11 @@
 <?php } else { ?>
 <a <?= rss_itemlist_anchor(); ?>></a>
 <?php } ?>
-
+<?php if (rss_itemlist_has_extractions()) { ?>
+<div id="feedaction" class="withmargin">
+	<?php rss_itemlist_prerender_callback() ?>
+</div>
+<?php } ?>
 <?= rss_itemlist_before_list(); ?>
 <?php rss_itemlist_feeds(); ?>
 <?= rss_itemlist_after_list(); ?>
