3dfed6747db3d80556010961271a4dabe18e158b
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-08-02 07:58:39 +0000

    Added a link to the SAJAX website to the powered-by footer span
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@700 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -3,7 +3,8 @@
 		<a href="http://devlog.gregarius.net/">Gregarius</a> <?= _VERSION_ ?><!-- $Revision: 577 $ -->
 		<?= LBL_FTR_POWERED_BY ?> <a href="http://php.net">PHP</a>, 
 		<a href="http://magpierss.sourceforge.net/">MagpieRSS</a>, 
-		<a href="http://sourceforge.net/projects/kses">kses</a></span>
+		<a href="http://sourceforge.net/projects/kses">kses</a>,
+		<a href="http://www.modernmethod.com/sajax/">SAJAX</a></span>
 	<span>
 		Tentatively valid <a title="Tentatively valid XHTML: the layout validates, but the actual content coming from the feeds I can't do very much."  href="http://validator.w3.org/check/referer">XHTML1.0</a>, 
 		<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS2.0</a>
