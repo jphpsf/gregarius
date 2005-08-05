667505c4ac1e7f11a5d3a91924939f07f59a8660
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-08-05 21:02:21 +0000

    tiny layout change
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@719 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/itemlist.php
+++ themes/default/itemlist.php
@@ -1,3 +1,8 @@
+<?php if (rss_itemlist_has_extractions()) { ?>
+<div id="feedaction" class="withmargin">
+	<?php rss_itemlist_prerender_callback() ?>
+</div>
+<?php } ?>
 <?php if ($title = rss_itemlist_title()) { ?>
 <h2<?= rss_itemlist_anchor(); ?>>
 <?php if ($icon = rss_itemlist_icon()) { ?>
@@ -7,11 +12,6 @@
 </h2>
 <?php } else { ?>
 <a <?= rss_itemlist_anchor(); ?>></a>
-<?php } ?>
-<?php if (rss_itemlist_has_extractions()) { ?>
-<div id="feedaction" class="withmargin">
-	<?php rss_itemlist_prerender_callback() ?>
-</div>
 <?php } ?>
 <?= rss_itemlist_before_list(); ?>
 <?php rss_itemlist_feeds(); ?>
