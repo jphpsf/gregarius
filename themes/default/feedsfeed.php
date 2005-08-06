14367b32175dbb9eae56093e441d99a315a9eeae
jmudry <jmudry@d826556c-87f9-0310-90b4-9554b5b11594>
2005-08-06 07:43:37 +0000

    A short php tag could still be found in gregarius's code. Corrected now.
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@720 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/feedsfeed.php
+++ themes/default/feedsfeed.php
@@ -2,7 +2,7 @@
 <li<?= rss_feeds_feed_li_class()?>>
 <?php if($icon = rss_feeds_feed_icon()) { ?>
 	<img src="<?= $icon ?>" class="favicon" alt="" />
-<? } ?>
+<?php } ?>
 	<a class="<?= rss_feeds_feed_class() ?>" title="<?= rss_feeds_feed_title_entities() ?>" href="<?= rss_feeds_feed_link() ?>"><?= rss_feeds_feed_title() ?></a>
 <?php if (($rdLbl = rss_feeds_feed_read_label()) != "") { ?>
 	<?= $rdLbl ?>
