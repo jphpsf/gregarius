3cb2083bd1e0b4d745e4a9b72f9bf3505ac3fcd8
mdodoo <mdodoo@d826556c-87f9-0310-90b4-9554b5b11594>
2005-08-12 17:29:34 +0000

    Updates language localization options.
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@736 d826556c-87f9-0310-90b4-9554b5b11594

--- schema.php
+++ schema.php
@@ -254,7 +254,7 @@ function setDefaults($key) {
 		"rss.output.channelcollapse"=>	array("true","true","boolean","Allow collapsing of channels on the main page. ",NULL),
 		"rss.output.usepermalinks"	=>		array("true","true","boolean","Display a permalink icon and allow linking a given item directly.",NULL),
 		"rss.config.markreadonupdate"=>	array("false","false","boolean","Mark all old unread feeds as read when updating if new unread feeds are found.",NULL),
-		"rss.output.lang"			=>		array("en,es,fr,se,0","en,fr,es,fr,0","enum","Language pack to use.",NULL),
+		"rss.output.lang"			=>		array("en,es,fr,dk,it,pt_BR,se,0","en,es,fr,dk,it,pt_BR,se,0","enum","Language pack to use.",NULL),
 		"rss.config.absoluteordering"=>	array("true","true","boolean","Allow ordering of channels and folders in the admin section. If false, channels and folders will be organized by their titles.",NULL),
 		"rss.config.robotsmeta"		=>		array("index,follow","index,follow","string","How should spiders crawl us? (see http://www.robotstxt.org/wc/meta-user.html for more info).",NULL),
 		"rss.config.serverpush"		=>		array("true","true","boolean","Use server push on update.php for a more user-friendly experience. This is only supported by Mozilla browsers (Netscape, Mozilla, Firefox,...) and Opera. These browsers will be autodetected. If you\'re not using one of these (you should) you can as well turn this off.",NULL),
