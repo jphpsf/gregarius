14375392f39498943caf67ee441f0eda8147c9d8
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-09-20 13:16:50 +0000

    adapted to the new external file model
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@887 d826556c-87f9-0310-90b4-9554b5b11594

--- plugins/newwindow.php
+++ plugins/newwindow.php
@@ -29,30 +29,48 @@
 /// Name: New window
 /// Author: Marco Bonetti
 /// Description: When active, this plugin will open off-site links in a new window
-/// Version: 1.6
+/// Version: 1.7
 
+function __new_window_js_register($js) {
+    $js[] = getPath(). RSS_PLUGINS_DIR . "/newwindow.php?nwjs";
+    return $js;
+}
 
-function __new_window_js($dummy) {
-?>
-<script type="text/javascript">
-// <!--
-if (document.getElementsByTagName) {
-	var items = document.getElementById("items");
-  	if (items) {
-   	var anchors = items.getElementsByTagName("a");
-   	for (var i=0; i<anchors.length; i++) {
-   		var anchor = anchors[i];
-   		if (anchor.href && (anchor.href.indexOf('<?php echo  $_SERVER['HTTP_HOST'] ?>') < 0)) {
-   			anchor.target = '_blank';
-   		}
-   	}
+function __new_window_init_js($dummy) {
+    echo "\n<script type=\"text/javascript\">\n"
+      ."<!--\n"
+      ."__new_window();\n"
+      ."-->\n"
+      ."</script>\n";
+    return $dummy;
+}
+
+if (isset($_REQUEST['nwjs'])) {
+
+	require_once('../init.php');
+    ETagHandler(md5("newwindow".'$Revision$'));
+    
+    ?>
+	function __new_window() {
+    if (document.getElementsByTagName) {
+        var items = document.getElementById("items");
+        if (items) {
+            var anchors = items.getElementsByTagName("a");
+            for (var i=0; i<anchors.length; i++) {
+                var anchor = anchors[i];
+                if (anchor.href && (anchor.href.indexOf('<?php echo  $_SERVER['HTTP_HOST'] ?>') < 0)) {
+                    anchor.target = '_blank';
+                }
+            }
+        }
+    }
 	}
-}
-// -->
-</script>
-<?php			 
-	return null;
+<?php
+    flush();
+    exit();
 }
 
-rss_set_hook('rss.plugins.bodyend','__new_window_js');
+rss_set_hook('rss.plugins.javascript','__new_window_js_register');
+rss_set_hook('rss.plugins.bodyend','__new_window_init_js');
+
 ?>
