75fb7c45269b2a4d1e0c2655364e7d6d9c08110d
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-10-26 19:51:05 +0000

    File header
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@983 d826556c-87f9-0310-90b4-9554b5b11594

--- extlib/favicon.php
+++ extlib/favicon.php
@@ -1,11 +1,41 @@
 <?php
+###############################################################################
+# Gregarius - A PHP based RSS aggregator.
+# Copyright (C) 2003 - 2005 Marco Bonetti
+#
+###############################################################################
+# This program is free software and open source software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
+# http://www.gnu.org/licenses/gpl.html
+#
+###############################################################################
+# E-mail:      mbonetti at users dot sourceforge dot net
+# Web page:    http://sourceforge.net/projects/gregarius
+###############################################################################
+
 require_once('../core.php');
+// Cache expires after 24 hours
 rss_bootstrap(true,'',24);
+
 if(!isset($_GET['url'])) {
 	exit();
 }
+
 $sql = "select data from " . getTable('cache') 
-	. " where cachetype='icon' and cachekey='" . rss_real_escape_string($_GET['url']) ."'";
+	. " where cachetype='icon' and cachekey='" 
+	. rss_real_escape_string($_GET['url']) ."'";
+	
 list($blob) = rss_fetch_row(rss_query($sql));
 if (!$blob) {
 	exit();
