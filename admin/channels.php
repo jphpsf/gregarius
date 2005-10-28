c0e1cec55f085adc225dc2372b4780e79cbc6848
sdcosta <sdcosta@d826556c-87f9-0310-90b4-9554b5b11594>
2005-10-28 04:44:20 +0000

    Fix for #240.
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@988 d826556c-87f9-0310-90b4-9554b5b11594

--- admin/channels.php
+++ admin/channels.php
@@ -258,7 +258,7 @@ function channel_admin() {
 				LBL_ADMIN_OPML_IMPORT_FEED_INFO,
 					htmlentities($label),"/$flabel")
 					. LBL_ADMIN_OK
-					. "&nbsp;[<a href=\"index.php?domain=".CST_ADMIN_DOMAIN_CHANNEL
+					. "&nbsp;[<a href=\"".getPath()."index.php?domain=".CST_ADMIN_DOMAIN_CHANNEL
 						."&amp;action=edit&amp;cid=$newCid\">"
 					. LBL_ADMIN_EDIT
 					."</a>]",
