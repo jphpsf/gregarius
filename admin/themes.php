b96c314f6ff13e48e13cc79c1b540096bee81f78
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2005-12-09 21:21:05 +0000

    cleanup
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@1077 d826556c-87f9-0310-90b4-9554b5b11594

--- admin/themes.php
+++ admin/themes.php
@@ -29,82 +29,76 @@
 function themes_admin() {
     return CST_ADMIN_DOMAIN_THEMES;
 }
-/*
-function plugin_options_admin() {
-    if (array_key_exists('plugin_name',$_REQUEST)) {
-        return CST_ADMIN_DOMAIN_PLUGIN_OPTIONS;
-    } else {
-        return CST_ADMIN_DOMAIN_PLUGINS;
-    }
-}
-*/
+
 function themes() {
-	$themes = getThemes();
-	if (isset($_REQUEST[CST_ADMIN_METAACTION]) && $_REQUEST[CST_ADMIN_METAACTION] == 'LBL_ADMIN_SUBMIT_CHANGES') {
-		if (isset($_REQUEST['value'])) {
-			$sql = "update " . getTable('config') . " set value_ = '". $_POST['value']."'"
-			." where key_='rss.output.theme'";
-			rss_query($sql);
-			$active_theme = $_POST['value'];
-		}	
-	} else {
-		$active_theme= getConfig('rss.output.theme');
-	}
+    $themes = getThemes();
+    if (isset($_REQUEST[CST_ADMIN_METAACTION]) && $_REQUEST[CST_ADMIN_METAACTION] == 'LBL_ADMIN_SUBMIT_CHANGES') {
+        if (isset($_POST['value'])) {
+            $sql = "update " . getTable('config') . " set value_ = '". $_POST['value']."'"
+                   ." where key_='rss.output.theme'";
+            rss_query($sql);
+            $active_theme = $_POST['value'];
+        }	 else {
+            $active_theme= getConfig('rss.output.theme');
+        }
+    } else {
+        $active_theme= getConfig('rss.output.theme');
+    }
     echo "<h2 class=\"trigger\">".LBL_ADMIN_THEMES."</h2>\n"
     ."<div id=\"admin_plugins\">\n";
 
 
     echo LBL_ADMIN_THEMES_GET_MORE;
 
-	
-    echo "<form method=\"post\" action=\"" .$_SERVER['PHP_SELF'] ."\">\n";	
-//	echo "<input type=\"hidden\" name=\"value\" value=\"\" />\n";
-	echo "<p><input type=\"hidden\" name=\"".CST_ADMIN_DOMAIN."\" value=\"".CST_ADMIN_DOMAIN_THEMES."\" /></p>\n";
 
-	echo "</p>\n<table id=\"plugintable\">\n<tr>\n"
-	."<th>".LBL_ADMIN_PLUGINS_HEADING_ACTION."</th>\n"
-	."<th>".LBL_ADMIN_PLUGINS_HEADING_NAME."</th>\n"
-	."<th>".LBL_ADMIN_PLUGINS_HEADING_VERSION."</th>\n"
-	."<th>".LBL_ADMIN_PLUGINS_HEADING_AUTHOR."</th>\n"
-	."<th>".LBL_ADMIN_PLUGINS_HEADING_DESCRIPTION."</th>\n"
-	."</tr>\n";
+    echo "<form method=\"post\" action=\"" .$_SERVER['PHP_SELF'] ."\">\n";
+    //	echo "<input type=\"hidden\" name=\"value\" value=\"\" />\n";
+    echo "<p><input type=\"hidden\" name=\"".CST_ADMIN_DOMAIN."\" value=\"".CST_ADMIN_DOMAIN_THEMES."\" /></p>\n";
 
-	$cntr = 0;
-	foreach ($themes as $entry => $theme) {
+    echo "</p>\n<table id=\"plugintable\">\n<tr>\n"
+    ."<th>".LBL_ADMIN_PLUGINS_HEADING_ACTION."</th>\n"
+    ."<th>".LBL_ADMIN_PLUGINS_HEADING_NAME."</th>\n"
+    ."<th>".LBL_ADMIN_PLUGINS_HEADING_VERSION."</th>\n"
+    ."<th>".LBL_ADMIN_PLUGINS_HEADING_AUTHOR."</th>\n"
+    ."<th>".LBL_ADMIN_PLUGINS_HEADING_DESCRIPTION."</th>\n"
+    ."</tr>\n";
 
-		extract($theme);
-		if (!$name) {
-			$name = $entry;
-		}
-		if ($url) {
-			$author = "<a href=\"$url\">$author</a>";
-		}
-		$active = ($entry ==  $active_theme);
-		echo "<tr class=\""
-		.(($cntr++ % 2 == 0)?"even":"odd")
-		.($active?" active":"")
-		."\">\n";
-		echo "<td class=\"cntr\">"
-		."<input type=\"radio\" name=\"value\" "
-		." id=\"_gregarius_theme_$entry\" value=\"$entry\" "
-		.($active?" checked=\"checked\"":"")
-		.(!$htmltheme?" disabled=\"disabled\"":"")
-		." />\n"
-		."</td>\n";
-		echo "<td><label for=\"_gregarius_theme_$entry\">".($name?$name:"&nbsp"). "</label></td>\n";
-		echo "<td class=\"cntr\">".($version?$version:"&nbsp"). "</td>\n";
-		echo "<td>"	.($author?$author:"&nbsp") . "</td>\n";
-		echo "<td>"	.($description?$description:"&nbsp"). "</td>\n";
+    $cntr = 0;
+    foreach ($themes as $entry => $theme) {
 
-		echo "</tr>\n";
-	}
-	echo "</table>\n";
+        extract($theme);
+        if (!$name) {
+            $name = $entry;
+        }
+        if ($url) {
+            $author = "<a href=\"$url\">$author</a>";
+        }
+        $active = ($entry ==  $active_theme);
+        echo "<tr class=\""
+        .(($cntr++ % 2 == 0)?"even":"odd")
+        .($active?" active":"")
+        ."\">\n";
+        echo "<td class=\"cntr\">"
+        ."<input type=\"radio\" name=\"value\" "
+        ." id=\"_gregarius_theme_$entry\" value=\"$entry\" "
+        .($active?" checked=\"checked\"":"")
+        .(!$htmltheme?" disabled=\"disabled\"":"")
+        ." />\n"
+        ."</td>\n";
+        echo "<td><label for=\"_gregarius_theme_$entry\">".($name?$name:"&nbsp"). "</label></td>\n";
+        echo "<td class=\"cntr\">".($version?$version:"&nbsp"). "</td>\n";
+        echo "<td>"	.($author?$author:"&nbsp") . "</td>\n";
+        echo "<td>"	.($description?$description:"&nbsp"). "</td>\n";
+
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
 
     echo "<p><input type=\"hidden\" name=\"". CST_ADMIN_METAACTION ."\" value=\"LBL_ADMIN_SUBMIT_CHANGES\"/>\n";
     echo "<input type=\"submit\" name=\"admin_plugin_submit_changes\" value=\"".LBL_ADMIN_SUBMIT_CHANGES."\" /></p>\n";
-	
-	echo "</form>\n";
-	echo "</div>\n";
+
+    echo "</form>\n";
+    echo "</div>\n";
 
 }
 
