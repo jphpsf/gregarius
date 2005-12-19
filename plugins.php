a50a7923997f523a5354e0cb14cc48eaf53899df
sdcosta <sdcosta@d826556c-87f9-0310-90b4-9554b5b11594>
2005-12-19 04:39:48 +0000

    Don't ask.
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@1096 d826556c-87f9-0310-90b4-9554b5b11594

--- plugins.php
+++ plugins.php
@@ -122,7 +122,7 @@ function rss_plugins_set_item_state($itemId, $bit_mask, $set
         // the bitmask has a zero in the spot (field) we want to change.
         if($set
           ) { // Set the value to the field to 1
-            $sql = "update " .getTable("item") ." set unread = unread | ~ ".  $bit_mask;
+            $sql = "update " .getTable("item") ." set unread = unread | ".  ~ $bit_mask;
         }
         else { // set the value of the field to 0
             $sql = "update " .getTable("item") ." set unread = unread & ". $bit_mask;
