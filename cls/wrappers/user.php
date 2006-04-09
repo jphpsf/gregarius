bbb76283391647b7edc0becaf0903659f19f01ae
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2006-04-09 22:03:15 +0000

    more inline docs
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@1427 d826556c-87f9-0310-90b4-9554b5b11594

--- cls/wrappers/user.php
+++ cls/wrappers/user.php
@@ -37,8 +37,9 @@ function rss_user_logout() {
 	$GLOBALS['rssuser'] -> logout();
 }
 
+// Legacy function name: use rss_user_check_user_level() instead of this crap
 function hidePrivate() {
-	return rss_user_level() < RSS_USER_LEVEL_PRIVATE;
+	return !rss_user_check_user_level(RSS_USER_LEVEL_PRIVATE);
 }
 
 function rss_user_check_user_level($level) {
