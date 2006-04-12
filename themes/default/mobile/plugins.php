acbc1e18c226cfb71e67d3df773b0a4f460f844c
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2006-04-12 18:46:46 +0000

    fix for mobile login
    git-svn-id: http://svn.gregarius.net/svn/trunk/rss@1434 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/mobile/plugins.php
+++ themes/default/mobile/plugins.php
@@ -46,11 +46,11 @@ function mobileLoginForm() {
 <body>
 <form method="post" action="<?php echo getPath(); ?>">
 <p><input type="hidden" name="media" value="mobile" />
-<label for="mobile_username">Username:</label>
-<input type="text" id="mobile_username" name="mobile_username" value="" /></p>
-<p><label for="mobile_password">Password:</label>
-<input type="password" name="mobile_password" id="mobile_password"  value="" /></p>
-<p><input type="submit" name="mobilelogin" value="Go" /></p>
+<label for="username">Username:</label>
+<input type="text" id="username" name="username" value="" /></p>
+<p><label for="password">Password:</label>
+<input type="password" name="password" id="password"  value="" /></p>
+<p><input type="submit" name="login" value="Go" /></p>
 </form>
 </body>
 </html>
@@ -60,21 +60,7 @@ exit();
 }
 
 if (isset($_REQUEST['mobilelogin'])) {
-	if (isset($_POST['mobile_username']) && isset($_POST['mobile_password'])) {
-		$un = rss_real_escape_string($_POST['mobile_username']);
-		$pw = md5($_POST['mobile_password']);
-		$dt = explode('|',__exp_login($un,$pw,null));
-		if ($dt[0] == RSS_USER_LEVEL_NOLEVEL) {
-			mobileLoginForm();
-		} else {
-			setUserCookie($dt[1], $dt[2]);
-			$_SESSION['mobile']=($dt[1] ."|" .$dt[2]);
-			echo "Hello, " . $dt[1] . ", <a href=\"".getPath()."?media=mobile\">continue...</a>";
-			flush();
-			exit();
-		}
-	} else {
-		mobileLoginForm();
-	}
+	mobileLoginForm();
 }
+
 ?>
