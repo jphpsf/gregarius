03fd084e32582e55241d80c5e184fccce98b97d7
sdcosta <sdcosta@d826556c-87f9-0310-90b4-9554b5b11594>
2006-06-09 22:43:40 +0000

    Export enclosures back into gregarius' rss feeds. #413
    This is not standards compliant since length and mime type are required and we do not store that. 
    http://blogs.law.harvard.edu/tech/rss#ltenclosuregtSubelementOfLtitemgt
    
    
    
    git-svn-id: http://svn.gregarius.net/svn/trunk/gregarius@1488 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/rss/item.php
+++ themes/default/rss/item.php
@@ -3,5 +3,8 @@
 		<link><?php echo rss_item_url(); ?></link>
 		<pubDate><?php echo rss_item_date_with_format('r'); ?></pubDate>
 		<guid><?php echo rss_item_url(); ?></guid>
+	    <?php if (rss_item_has_enclosure()) { ?>
+		<enclosure url="<?php echo rss_item_enclosure(); ?>" length="" type="" />
+	    <?php } ?>
 		<content:encoded><![CDATA[	<?php echo rss_item_content(); ?> ]]></content:encoded>
 </item>
