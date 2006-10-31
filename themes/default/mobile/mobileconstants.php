da71f5f4190ff19149b54fd1861712983c86ac9b
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2006-10-31 17:12:04 +0000

    git-svn-id: http://svn.gregarius.net/svn/trunk/gregarius@1616 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/mobile/mobileconstants.php
+++ themes/default/mobile/mobileconstants.php
@@ -3,6 +3,6 @@
 define('DEFAULT_MOBILE_FRONTPAGE_NUMITEMS',10);
 //default override value for the number of items on a channel page
 define('DEFAULT_MOBILE_FRONTPAGE_ITEMSINCHANNELVIEW',10);
-define('DEFAULT_MOBILE_OUTPUT_MAXLENGTH',200);
+define('DEFAULT_MOBILE_OUTPUT_MAXLENGTH',0);
 define('DEFAULT_MOBILE_CONTENT_STRIP_IMAGES','false');
 ?>
