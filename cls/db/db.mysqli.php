aafc9df7374a17563e0b5cabd1e2fee218dbc0ba
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2007-04-05 13:40:25 +0000

    we can avoid using procedural style here
    git-svn-id: http://svn.gregarius.net/svn/trunk/gregarius@1701 d826556c-87f9-0310-90b4-9554b5b11594

--- cls/db/db.mysqli.php
+++ cls/db/db.mysqli.php
@@ -30,111 +30,114 @@ require_once(dirname(__FILE__) . '/db.php');
 
 class MysqliDB extends DB {
 
-    // we could as well set this to private and use PHP5-syntax __construct for 
+    // we could as well set this to private and use PHP5-syntax __construct for
     // the constructor, as Mysqli is only supported by PHP5, afaik.
-    
-	var $_db;
 
-	function  MysqliDB() {
-		parent::DB();
-	}
+    var $_db;
 
-	
-	function DBConnect($dbserver, $dbuname, $dbpass) {
-    $this -> _db = new mysqli($dbserver, $dbuname, $dbpass);
+    function  MysqliDB() {
+        parent::DB();
+    }
 
-		if(mysqli_connect_errno()) {
-			  die( "<h1>Error connecting to the database!</h1>\n"
-					 ."<p>Have you edited dbinit.php and correctly defined "
-					 ."the database username and password?</p>\n" );		  
-		}
-	}
-	
-	function DBSelectDB($dbname) {
-		if (!$this -> _db -> select_db($dbname)) {
-			  die( "<h1>Error selecting the database!</h1>\n"
-					 ."<p>Does your database exist?"
-					 . "Have you edited dbinit.php and correctly defined "
-					 ."the database username and password?</p>\n"
-					 ."<p>Refer to the <a href=\"INSTALL\">INSTALL</a> document "
-					 ."if in doubt</p>\n" );
-		}
-	}
-	
-	function rss_query ($query, $dieOnError=true, $preventRecursion=false) {
-		 $ret =  $this -> _db -> query($query);
-	
-		 if ($error = $this -> rss_sql_error()) {
-			  $errorString = $this -> rss_sql_error_message();
-		 }
-	
-		 // if we got a missing table error, look for missing tables in the schema
-		 // and try to create them
-		 if ($error == 1146 && !$preventRecursion && $dieOnError) {
-		 		require_once(dirname(__FILE__) . '/../../init.php');
-		 		rss_require('schema.php');
-			  checkSchema();
-			  return $this -> rss_query ($query, $dieOnError, true);
-		 } elseif ($error == 1054 && !$preventRecursion && $dieOnError) {
-			  if (preg_match("/^[^']+'([^']+)'.*$/",$errorString,$matches)) {
-			  	require_once(dirname(__FILE__) . '/../../init.php');
-			  	rss_require('schema.php');
-					checkSchemaColumns($matches[1]);
-					return $this -> rss_query ($query, $dieOnError, true);
-			  }
-		 }
-	
-		if ($error && $dieOnError) {
-			  die ("<p>Failed to execute the SQL query <pre>$query</pre> </p>"
-					 ."<p>Error $error: $errorString</p>");
-		 }
-		 return $ret;
-	}
-	
-	function rss_fetch_row($rs) {
-		return  $rs -> fetch_row();
-	}
-	
-	function rss_fetch_assoc($rs) {
-		return $rs -> fetch_assoc();
-	}
-	function rss_num_rows($rs) {
-		return $rs -> num_rows;
-	}
-	
-	function rss_sql_error() {
-		 return $this -> _db -> errno;
-	}
-	
-	function rss_sql_error_message () {
-		 return $this -> _db -> error;
-	}
-	
-	function rss_insert_id() {
-		return $this -> _db -> insert_id;
-	}
-	
-	function rss_real_escape_string($string) {
-		 if (method_exists($this -> _db, 'real_escape_string')) {
-			  return $this -> _db -> real_escape_string($string);
-		 } elseif (method_exists($this -> _db, 'escape_string')) {
-			  return $this -> _db -> escape_string($string);
-		 } else {
-			  die("<p class=\"error\">Your PHP version doesn't meet Gregarius' minimal requirements, please consider upgrading!</p>");
-		 }
-	}
-	
-	function rss_is_sql_error($kind) {
-		switch ($kind) {
-			case RSS_SQL_ERROR_NO_ERROR:
-				return ($this -> rss_sql_error() == 0);
-				break;
-			case RSS_SQL_ERROR_DUPLICATE_ROW:
-				return ($this -> rss_sql_error() == 1062);
-				break;
-			default:
-				return false;
-		}
-	}
+
+    function DBConnect($dbserver, $dbuname, $dbpass) {
+        $this -> _db = new mysqli($dbserver, $dbuname, $dbpass);
+
+        if(!$this -> _db) {
+            die( "<h1>Error connecting to the database!</h1>\n"
+                 ."<p>Have you edited dbinit.php and correctly defined "
+                 ."the database username and password?</p>\n" );
+        }
+    }
+
+    function DBSelectDB($dbname) {
+        if (!$this -> _db -> select_db($dbname)) {
+            die( "<h1>Error selecting the database!</h1>\n"
+                 ."<p>Does your database exist?"
+                 . "Have you edited dbinit.php and correctly defined "
+                 ."the database username and password?</p>\n"
+                 ."<p>Refer to the <a href=\"INSTALL\">INSTALL</a> document "
+                 ."if in doubt</p>\n" );
+        }
+    }
+
+    function rss_query ($query, $dieOnError=true, $preventRecursion=false) {
+        $ret =  $this -> _db -> query($query);
+
+        if ($error = $this -> rss_sql_error()) {
+            $errorString = $this -> rss_sql_error_message();
+        }
+
+        // if we got a missing table error, look for missing tables in the schema
+        // and try to create them
+        if ($error == 1146 && !$preventRecursion && $dieOnError) {
+            require_once(dirname(__FILE__) . '/../../init.php');
+            rss_require('schema.php');
+            checkSchema();
+            return $this -> rss_query ($query, $dieOnError, true);
+        }
+        elseif ($error == 1054 && !$preventRecursion && $dieOnError) {
+            if (preg_match("/^[^']+'([^']+)'.*$/",$errorString,$matches)) {
+                require_once(dirname(__FILE__) . '/../../init.php');
+                rss_require('schema.php');
+                checkSchemaColumns($matches[1]);
+                return $this -> rss_query ($query, $dieOnError, true);
+            }
+        }
+
+        if ($error && $dieOnError) {
+            die ("<p>Failed to execute the SQL query <pre>$query</pre> </p>"
+                 ."<p>Error $error: $errorString</p>");
+        }
+        return $ret;
+    }
+
+    function rss_fetch_row($rs) {
+        return  $rs -> fetch_row();
+    }
+
+    function rss_fetch_assoc($rs) {
+        return $rs -> fetch_assoc();
+    }
+    function rss_num_rows($rs) {
+        return $rs -> num_rows;
+    }
+
+    function rss_sql_error() {
+        return $this -> _db -> errno;
+    }
+
+    function rss_sql_error_message () {
+        return $this -> _db -> error;
+    }
+
+    function rss_insert_id() {
+        return $this -> _db -> insert_id;
+    }
+
+    function rss_real_escape_string($string) {
+        if (method_exists($this -> _db, 'real_escape_string')) {
+            return $this -> _db -> real_escape_string($string);
+        }
+        elseif (method_exists($this -> _db, 'escape_string')) {
+            return $this -> _db -> escape_string($string);
+        }
+        else {
+            die("<p class=\"error\">Your PHP version doesn't meet Gregarius' minimal requirements, please consider upgrading!</p>");
+        }
+    }
+
+    function rss_is_sql_error($kind) {
+        switch ($kind) {
+        case RSS_SQL_ERROR_NO_ERROR:
+                return ($this -> rss_sql_error() == 0);
+            break;
+        case RSS_SQL_ERROR_DUPLICATE_ROW:
+            return ($this -> rss_sql_error() == 1062);
+            break;
+        default:
+            return false;
+        }
+    }
 }
 ?>
