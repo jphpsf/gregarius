4990ec3e0f720aba1da827e4533fd59329a004c4
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2007-08-25 15:17:03 +0000

    Add author tags to exported RSS feeds, too
    git-svn-id: http://svn.gregarius.net/svn/trunk/gregarius@1749 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/rss/item.php
+++ themes/default/rss/item.php
@@ -6,5 +6,8 @@
 	    <?php if (rss_item_has_enclosure()) { ?>
 		<enclosure url="<?php echo rss_item_enclosure(); ?>" length="" type="" />
 	    <?php } ?>
+		<?php if ($GLOBALS['rss']->currentItem->author ) { ?>
+		<author><?php echo $GLOBALS['rss']->currentItem->author; ?></author>		
+		<?php } ?>
 		<content:encoded><![CDATA[	<?php echo rss_item_content(); ?> ]]></content:encoded>
 </item>
