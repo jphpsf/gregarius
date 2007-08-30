b53b2e4ee150eae19b5f0c67575e38f40de6c356
cfriesen <cfriesen@d826556c-87f9-0310-90b4-9554b5b11594>
2007-08-30 00:16:25 +0000

    Add the unread count to the mobile feed list
    git-svn-id: http://svn.gregarius.net/svn/trunk/gregarius@1752 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/mobile/feedsfeed.php
+++ themes/default/mobile/feedsfeed.php
@@ -1,1 +1,1 @@
-<li><a href="<?php echo rss_feeds_feed_link();  ?>"><?php echo rss_feeds_feed_title(); ?></a></li>
+<li><a href="<?php echo rss_feeds_feed_link();  ?>"><?php echo rss_feeds_feed_title(); ?></a><?php if (($rdLbl = rss_feeds_feed_read_label()) != "") { ?> <?php echo $rdLbl; ?> <?php } ?>
