cd9026f2badd1cb86ff9c40cdfd76129cb87b026
mbonetti <mbonetti@d826556c-87f9-0310-90b4-9554b5b11594>
2008-07-26 09:18:54 +0000

    getting ready for the next release
    git-svn-id: http://svn.gregarius.net/svn/trunk/gregarius@1789 d826556c-87f9-0310-90b4-9554b5b11594

--- themes/default/web/feedsfeed.php
+++ themes/default/web/feedsfeed.php
@@ -2,10 +2,7 @@
 <?php if($icon = rss_feeds_feed_icon()) { ?>
 	<img src="<?php echo $icon; ?>" class="favicon" alt="" />
 <?php } ?>
-<?php if(!getConfig('rss.config.restrictrefresh')) { ?>
-<?php echo "<a href=\"" . getPath() . "update.php?cid=" . rss_feeds_feed_id() . "\"><img src=\"" . getExternalThemeFile("media/arrow_refresh.png") . "\" width=\"16\" height=\"16\"></a>"; ?>
-<?php } ?>
-	<a class="<?php echo rss_feeds_feed_class(); ?>" title="<?php echo rss_feeds_feed_description_entities(); ?>" href="<?php echo rss_feeds_feed_link();  ?>"><?php echo rss_feeds_feed_title(); ?></a>
+<a class="<?php echo rss_feeds_feed_class(); ?>" title="<?php echo rss_feeds_feed_description_entities(); ?>" href="<?php echo rss_feeds_feed_link();  ?>"><?php echo rss_feeds_feed_title(); ?></a>
 <?php if (($rdLbl = rss_feeds_feed_read_label()) != "") { ?>
 	<?php echo $rdLbl; ?>
 <?php } ?>
